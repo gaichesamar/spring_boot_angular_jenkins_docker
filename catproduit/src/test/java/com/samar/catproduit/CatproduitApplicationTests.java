4b8e82b2f86961c50841c35e4392bd8975200af3
samar gaiche <samargaiche82@gmail.com>
2023-02-23 12:16:00 +0100

    first commit

---
+++ catproduit/src/test/java/com/samar/catproduit/CatproduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samar.catproduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatproduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ catproduit/src/test/java/com/samar/catproduit/web/categorieRestControllerTest.java
@@ -0,0 +1,7 @@
+package com.samar.catproduit.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class categorieRestControllerTest {
+
+}
